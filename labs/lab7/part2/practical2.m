dbac167a73736a6d35c6d13f4235a41a84418d60
Mincong Zhang <mincong.zhang@hotmail.com>
2014-04-27 23:18:19 +0100

    Augmented Reality/Pinhole camera

--- labs/lab7/part2/practical2.m
+++ labs/lab7/part2/practical2.m
@@ -38,7 +38,7 @@ xImCart = projectiveCamera(K,T,XCart);
 % TO DO Add noise to the pixel positions to simulate having to find these points in a noisy
 %image. Store the results back in xImCart.  
 %The noise should have standard deviation of one pixel in each direction.
-
+xImCart = xImCart+1*randn(size(xImCart));
 
 %Now we will take the image points and the known positions on the card and try to
 %estimate the extrinsic matrix using the algorithm discussed in the lecture. 
@@ -60,17 +60,23 @@ function xImCart = projectiveCamera(K,T,XCart);
 xImCart = [];
 
 %TO DO convert Cartesian 3d points XCart to homogeneous coordinates XHom
+XHom = [XCart;ones(1,size(XCart,2))];
+
 
 %TO DO apply extrinsic matrix to XHom to move to frame of reference of
 %camera
+xCamHom = T*XHom;
 
 %TO DO project points into normalized camera coordinates xCamHom by (achieved by
 %removing fourth row)
+%Because K is 3x3 matrix
+xCamHom(4,:) = [];
 
 %TO DO move points to image coordinates xImHom by applying intrinsic matrix
+xImHom = K*xCamHom;
 
 %TO DO convert points back to Cartesian coordinates xImCart
-
+xImCart = xImHom(1:2,:)./repmat(xImHom(3,:),2,1);
 
 
 %==========================================================================
@@ -83,33 +89,105 @@ xImCart = [];
 function T = estimatePlanePose(xImCart,XCart,K)
 
 %replace this
-T = [];
+%T = [];
 
 %TO DO Convert Cartesian image points xImCart to homogeneous representation
-%xImHom
+xImHom = [xImCart;ones(1,size(xImCart,2))];
 
 %TO DO Convert image co-ordinates xImHom to normalized camera coordinates
-%xCamHom
+xCamHom = inv(K)*xImHom;
 
 %TO DO Estimate homography H mapping homogeneous (x,y)
 %coordinates of positions in real world to xCamHom.  Use the routine you wrote for
 %Practical 1B.
+xCamCart = xCamHom;
+xCamCart(end,:) = []; %change to Cartisian 
+H = calcBestHomography(XCart, xCamCart);
 
 %TO DO Estimate first two columns of rotation matrix R from the first two
 %columns of H using the SVD
+R = H(:,1:2);
+[U,S,V] = svd(R);
+R_hat = U*[1,0;0,1;0,0]*V';
 
 %TO DO Estimate the third column of the rotation matrix by taking the cross
 %product of the first two columns
+R_hat = [R_hat cross(R_hat(:,1),R_hat(:,2))];
 
 %TO DO Check that the determinant of the rotation matrix is positive - if
 %not then multiply last column by -1.
+if(det(R_hat)<=0)
+    R_hat(end,:) = -1*R_hat(:,end); 
+end
 
 %TO DO Estimate the translation t by finding the appropriate scaling factor k
 %and applying it to the third colulmn of H
+k = sum(sum(R_hat(:,1:2)./H(:,1:2)));
+t = k.*H(:,4); %fourth column
+t = t/6; %take the average ratio
 
 %TO DO Check whether t_z is negative - if it is then multiply t by -1 and
 %the first two columns of R by -1.
+t_z = t(end);
+if (t_z<0)
+    t = -1*t;
+    R_hat(:,1:2) = -1*R_hat(:,1:2);
+end
+
+%method from the textbook cannot be used, because H has a zero element, the scaling
+%factor cannot be calculate in the textbook way.
 
 %assemble transformation into matrix form
-%T  = [R t;0 0 0 1];
+T  = [R_hat t;0 0 0 1];
 
+
+%==========================================================================
+function H = calcBestHomography(pts1Cart, pts2Cart)
+
+%should apply direct linear transform (DLT) algorithm to calculate best
+%homography that maps the points in pts1Cart to their corresonding matchin in 
+%pts2Cart
+
+%****TO DO ****: replace this
+%**** TO DO ****;
+%first turn points to homogeneous
+pts1Hom = [pts1Cart; ones(1,size(pts1Cart,2))];
+pts2Hom = [pts2Cart; ones(1,size(pts2Cart,2))];
+
+%then construct A matrix
+[m n] = size(pts1Hom);
+pts1Hom = pts1Hom';
+pts2Hom = pts2Hom';
+Y = pts2Hom(:,2);
+X = pts2Hom(:,1);
+A1 = zeros(n*2,4);
+A2 = zeros(n*2,4);
+A3 = zeros(n*2,4);
+for i = 1:n
+    A1(2*i-1,:) = pts1Hom(i,:);
+    A2(2*i,:) = pts1Hom(i,:);
+    A3(2*i-1,:) = -X(i)*pts1Hom(i,:);
+    A3(2*i,:) = -Y(i)*pts1Hom(i,:);
+end
+A = [A1 A2 A3];
+
+%solve Ah = 0 by calling
+%h = solveAXEqualsZero(A); (you have to write this routine too - see below)
+h = solveAXEqualsZero(A);
+
+%reshape h into the matrix H
+H = reshape(h,4,3)';
+
+%Beware - when you reshape the (9x1) vector x to the (3x3) shape of a homography, you must make
+%sure that it is reshaped with the values going first into the rows.  This
+%is not the way that the matlab command reshape works - it goes columns
+%first.  In order to resolve this, you can reshape and then take the
+%transpose
+
+
+%==========================================================================
+function x = solveAXEqualsZero(A);
+
+%****TO DO **** Write this routine 
+[U,S,V] = svd(A);
+x = V(:,end);
--- labs/lab7/part2/practical2b.m
+++ labs/lab7/part2/practical2b.m
@@ -30,21 +30,160 @@ plot(xImCart(1,:),xImCart(2,:),'r.','MarkerSize',10);
        
 %TO DO Use your routine to calculate TEst, the extrinsic matrix relating the
 %plane position to the camera position.
-
-
-
+TEst = estimatePlanePose(xImCart,XCart,K);
+xImCart = projectiveCamera(K,TEst,XCart)
 
 %define 3D points of plane
 XWireFrameCart = [-50 -50  50  50 -50 -50  50  50;...
                    50 -50 -50  50  50 -50 -50  50;...
                     0   0   0   0 -100 -100 -100 -100];
+                
+XImWireFrameCart = projectiveCamera(K,TEst,XWireFrameCart);
 
 %TO DO Draw a wire frame cube, by projecting the vertices of a 3D cube
 %through the projective camera and drawing lines betweeen the resulting 2d image
 %points
 
+%draw lines between each pair of points
+nPoint = size(XWireFrameCart,2)
+for i = 1:nPoint
+    for j = 1:nPoint
+        %Plot a line between two points that have distance of 100 (is one edge of the cube)
+        if  ( sum(abs(XWireFrameCart(:,i) - XWireFrameCart(:,j)))==100 ) 
+            plot([XImWireFrameCart(1,i) XImWireFrameCart(1,j)],[XImWireFrameCart(2,i) XImWireFrameCart(2,j)],'g-');
+            hold on;
+        end
+    end
+end;
+
+
+
 %QUESTIONS TO THINK ABOUT...
 
 %Do the results look realistic?
 %If not, then what factors do you think might be causing this?
+%estimating error 
+%Shading? shadow
 
+
+%==========================================================================
+%==========================================================================
+
+%goal of function is to project points in XCart through projective camera
+%defined by intrinsic matrix K and extrinsic matrix T.
+function xImCart = projectiveCamera(K,T,XCart);
+
+%replace this
+xImCart = [];
+
+%TO DO convert Cartesian 3d points XCart to homogeneous coordinates XHom
+XHom = [XCart;ones(1,size(XCart,2))];
+
+
+%TO DO apply extrinsic matrix to XHom to move to frame of reference of
+%camera
+xCamHom = T*XHom;
+
+%TO DO project points into normalized camera coordinates xCamHom by (achieved by
+%removing fourth row)
+%Because K is 3x3 matrix
+xCamHom(4,:) = [];
+
+%TO DO move points to image coordinates xImHom by applying intrinsic matrix
+xImHom = K*xCamHom;
+
+%TO DO convert points back to Cartesian coordinates xImCart
+xImCart = xImHom(1:2,:)./repmat(xImHom(3,:),2,1);
+
+
+%==========================================================================
+%==========================================================================
+
+function T = estimatePlanePose(xImCart,XCart,K)
+
+
+%Convert Cartesian image points xImCart to homogeneous representation
+xImHom = [xImCart;ones(1,size(xImCart,2))];
+
+%Convert image co-ordinates xImHom to normalized camera coordinates
+xCamHom = inv(K)*xImHom;
+
+%Estimate homography H mapping homogeneous (x,y)
+%coordinates of positions in real world to xCamHom.  Use the routine you wrote for
+%Practical 1B.
+H = calcBestHomography(XCart, xCamHom);
+
+%Estimate first two columns of rotation matrix R from the first two
+%columns of H using the SVD
+R = H(:,1:2);
+[U,S,V] = svd(R);
+R_hat = U*[1,0;0,1;0,0]*V';
+
+%Estimate the third column of the rotation matrix by taking the cross
+%product of the first two columns
+R_hat = [R_hat cross(R_hat(:,1),R_hat(:,2))];
+
+%Check that the determinant of the rotation matrix is positive - if
+%not then multiply last column by -1.
+if(det(R_hat)<=0)
+    R_hat(end,:) = -1*R_hat(:,end); 
+end
+
+%Estimate the translation t by finding the appropriate scaling factor k
+%and applying it to the third colulmn of H
+k = sum(sum(R_hat(:,1:2)./H(:,1:2)));
+t = k.*H(:,4); %fourth column
+t = t/6; %take the average ratio
+
+%Check whether t_z is negative - if it is then multiply t by -1 and
+%the first two columns of R by -1.
+t_z = t(end);
+if (t_z<0)
+    t = -1*t;
+    R_hat(:,1:2) = -1*R_hat(:,1:2);
+end
+
+%assemble transformation into matrix form
+T  = [R_hat t;0 0 0 1];
+
+
+%==========================================================================
+function H = calcBestHomography(pts1Cart, pts2Cart)
+
+%should apply direct linear transform (DLT) algorithm to calculate best
+%homography that maps the points in pts1Cart to their corresonding matchin in 
+%pts2Cart
+
+%first turn points to homogeneous
+pts1Hom = [pts1Cart; ones(1,size(pts1Cart,2))];
+pts2Hom = [pts2Cart; ones(1,size(pts2Cart,2))];
+
+%then construct A matrix
+[m n] = size(pts1Hom);
+pts1Hom = pts1Hom';
+pts2Hom = pts2Hom';
+Y = pts2Hom(:,2);
+X = pts2Hom(:,1);
+A1 = zeros(n*2,4);
+A2 = zeros(n*2,4);
+A3 = zeros(n*2,4);
+for i = 1:n
+    A1(2*i-1,:) = pts1Hom(i,:);
+    A2(2*i,:) = pts1Hom(i,:);
+    A3(2*i-1,:) = -X(i)*pts1Hom(i,:);
+    A3(2*i,:) = -Y(i)*pts1Hom(i,:);
+end
+A = [A1 A2 A3];
+
+%solve Ah = 0 by calling
+%h = solveAXEqualsZero(A); (you have to write this routine too - see below)
+h = solveAXEqualsZero(A);
+
+%reshape h into the matrix H
+H = reshape(h,4,3)';
+
+
+%==========================================================================
+function x = solveAXEqualsZero(A);
+[U,S,V] = svd(A);
+x = V(:,end);
